deb57a2079bd97ee294a6a6ea2f315164cc034c7
Rob Botham <r.a.botham@gmail.com>
2026-05-07 15:35:38 -0400

    chore: reorganize imports and define module exports

--- src/quickdb/__init__.py
+++ src/quickdb/__init__.py
@@ -1,2 +1,17 @@
-def hello() -> str:
-    return 'Hello from quickdb!'
+from quickdb.core import (
+    SQLDatabase,
+    SQLTable,
+    SQLServer,
+    SQLQueryManager,
+    read_query,
+    resolve_project_root,
+)
+
+__all__ = [
+    'SQLDatabase',
+    'SQLTable',
+    'SQLServer',
+    'SQLQueryManager',
+    'read_query',
+    'resolve_project_root',
+]
